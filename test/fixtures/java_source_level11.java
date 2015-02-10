9cfb79ab58ffa78ceabf31ed444c911dd59dbcdf
JESUS ESPEJO ARNAS <jespejo@externos.bankia.com>
2015-02-10 13:29:33 +0100

    modified tests

--- test/fixtures/java_source_level11.java
+++ test/fixtures/java_source_level11.java
@@ -1,5 +1,5 @@
 /**
- * This is the comment for testing grunt java2html plugin 
+ * This is the comment for testing grunt java2html plugin y en Español ESPAÑOL 
  */
 public List<String> myLevel11Function() {
 	// Inline comment for testing purposes
